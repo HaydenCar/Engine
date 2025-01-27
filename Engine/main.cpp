5f2ab30dd3951ea2d239ccd6a91aae4f312c249d
Hayden Carr <HaydenCarrBusiness@gmail.com>
2025-01-27 17:44:29 +0000

    Initial Commit

---
+++ Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Engine
+//
+//  Created by Hayden Carr on 27/01/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
